4b7bda2f7ba1ac5ba136caeca78610cd8ced8e3d
Danilo Leon <daniloleon15@gmail.com>
2020-09-15 17:04:45 -0300

    commit #2, se agrego el modelo cliete

--- trabajo_final_sigatam/apps/clientes/models.py
+++ trabajo_final_sigatam/apps/clientes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class  Cliente (models.Model):
+   id_cliente = models.AutoField (primary_key= True)
+   nombre = models.CharField(max_length = 220, blank = False, null = False)
+   apellido = models.CharField(max_length = 220, blank = False, null = False)
+   dni = models.IntegerField ()
+   direccion = models.CharField(max_length = 220, blank = False, null = False)
+   correo = models.CharField(max_length = 100, blank = False, null = False)
+   edad = models.IntegerField ()
+   
