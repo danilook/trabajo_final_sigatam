25f568a921fb095fe2f2411d11b8354e32dcd53d
Danilo Leon <daniloleon15@gmail.com>
2020-09-15 18:01:29 -0300

    commit #4, se modifico app clientes

--- trabajo_final_sigatam/apps/clientes/models.py
+++ trabajo_final_sigatam/apps/clientes/models.py
@@ -9,4 +9,9 @@ class  Cliente (models.Model):
    direccion = models.CharField(max_length = 220, blank = False, null = False)
    correo = models.CharField(max_length = 100, blank = False, null = False)
    edad = models.IntegerField ()
-   
+   class Meta:
+      verbose_name = 'Cliente'
+      verbose_name_plural = 'Clientes'
+      ordering = ['nombre']
+   def  __str__(self):
+      return self.nombre
