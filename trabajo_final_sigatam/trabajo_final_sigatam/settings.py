4ee226d2efe423b06cbd085ee8f17600e7a26039
Danilo Leon <daniloleon15@gmail.com>
2020-09-20 19:13:15 -0300

    commit #6 , se agrego index.html y se modifico, setting.py y app clientes

--- trabajo_final_sigatam/trabajo_final_sigatam/settings.py
+++ trabajo_final_sigatam/trabajo_final_sigatam/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'trabajo_final_sigatam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
