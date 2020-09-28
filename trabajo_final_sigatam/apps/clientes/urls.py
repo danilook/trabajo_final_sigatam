f97596f2a8432605caf8f7599f9cbc8b206605d0
Danilo Leon <daniloleon15@gmail.com>
2020-09-27 21:09:09 -0300

    commit #8, se agrego la vista de funcion listar clientes

--- trabajo_final_sigatam/apps/clientes/urls.py
+++ trabajo_final_sigatam/apps/clientes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import crearCliente
+from .views import crearCliente,listarCliente
 
 
 urlpatterns =[
-    path('crear_cliente/', crearCliente, name =  'crear_cliente')
+    path('crear_cliente/', crearCliente, name =  'crear_cliente'),
+    path('listar_cliente/', listarCliente, name= 'listar_cliente')
+
 ]
--- trabajo_final_sigatam/apps/clientes/views.py
+++ trabajo_final_sigatam/apps/clientes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .forms import ClienteForm
-
+from .models import Cliente
 def Home(request):
     return render (request,'index.html')
 
-def crearCliente(request):
+def crearCliente(request):                              #Funcion para crear un cliente #
     if request.method == 'POST':
         cliente_form = ClienteForm(request.POST)
         if cliente_form.is_valid():
@@ -13,3 +13,7 @@ def crearCliente(request):
     else:
         cliente_form = ClienteForm()
     return render (request,'clientes/crear_cliente.html',{'cliente_form':cliente_form})
+
+def listarCliente(request):                                #funcion para listar un cliente #
+    clientes = Cliente.objects.all()                   #traemos todos los clientes registrados en la base de datos#
+    return render(request,'clientes/listar_cliente.html',{'clientes':clientes})   #retornamos para renderizar en un template#
