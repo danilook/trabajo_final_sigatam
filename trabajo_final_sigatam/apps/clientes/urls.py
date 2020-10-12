9fe6bc2b8ef04fc71e8062820fef6e16232aebaa
Danilo Leon <daniloleon15@gmail.com>
2020-10-12 17:15:15 -0300

    se agrego la funcionalidad editar cliente

--- trabajo_final_sigatam/apps/clientes/urls.py
+++ trabajo_final_sigatam/apps/clientes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import crearCliente,listarCliente
+from .views import crearCliente,listarCliente,editarCliente
 
 
 urlpatterns =[
     path('crear_cliente/', crearCliente, name =  'crear_cliente'),
-    path('listar_cliente/', listarCliente, name= 'listar_cliente')
-
+    path('listar_cliente/', listarCliente, name= 'listar_cliente'),
+    path('editar_cliente/<int:dni>',editarCliente, name= 'editar_cliente')
 ]
--- trabajo_final_sigatam/apps/clientes/views.py
+++ trabajo_final_sigatam/apps/clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ClienteForm
 from .models import Cliente
 def Home(request):
@@ -17,3 +17,14 @@ def crearCliente(request):                              #Funcion para crear un c
 def listarCliente(request):                                #funcion para listar un cliente #
     clientes = Cliente.objects.all()                   #traemos todos los clientes registrados en la base de datos#
     return render(request,'clientes/listar_cliente.html',{'clientes':clientes})   #retornamos para renderizar en un template#
+
+def editarCliente(request,id):
+    cliente = Cliente.objects.get(id = id)
+    if request.method == 'GET' :
+        cliente_form = ClienteForm (instance= cliente)
+    else:
+        cliente_form = ClienteForm (request.POST, instance = cliente)
+        if cliente_form.is_valid():
+            cliente_form.save()
+        redirect('index')
+    return render(request,'clientes/crear_cliente.html',{'cliente_form': cliente_form})
