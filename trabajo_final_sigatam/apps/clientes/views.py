727e62154d8ab5dddb3ceeb5a5cfe0cb0a190678
Danilo Leon <daniloleon15@gmail.com>
2020-10-12 20:26:22 -0300

    commit #10, se completo la funcionalidad editar clientes

--- trabajo_final_sigatam/apps/clientes/views.py
+++ trabajo_final_sigatam/apps/clientes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.core.exceptions import ObjectDoesNotExist  # se importa una exeptions #
 from .forms import ClienteForm
 from .models import Cliente
 def Home(request):
@@ -18,13 +19,19 @@ def listarCliente(request):                                #funcion para listar 
     clientes = Cliente.objects.all()                   #traemos todos los clientes registrados en la base de datos#
     return render(request,'clientes/listar_cliente.html',{'clientes':clientes})   #retornamos para renderizar en un template#
 
-def editarCliente(request,id):
-    cliente = Cliente.objects.get(id = id)
-    if request.method == 'GET' :
-        cliente_form = ClienteForm (instance= cliente)
-    else:
-        cliente_form = ClienteForm (request.POST, instance = cliente)
-        if cliente_form.is_valid():
-            cliente_form.save()
-        redirect('index')
-    return render(request,'clientes/crear_cliente.html',{'cliente_form': cliente_form})
+def editarCliente(request,dni):
+    cliente_form = None
+    error = None
+    try:
+        cliente = Cliente.objects.get(dni = dni)
+        if request.method == 'GET' :
+            cliente_form = ClienteForm (instance= cliente)
+        else:
+            cliente_form = ClienteForm (request.POST, instance = cliente)
+            if cliente_form.is_valid():
+                cliente_form.save()
+            return  redirect('index')
+    except   ObjectDoesNotExist  as e:
+      error = e
+
+    return render(request,'clientes/crear_cliente.html',{'cliente_form': cliente_form, 'error':error})
